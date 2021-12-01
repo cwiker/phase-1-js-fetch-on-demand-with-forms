c26fa73b018a19aedccc791cfa07a4c3131161ce
Chris Wiker <cwiker7@gmail.com>
2021-12-01 12:59:40 -0500

    commit tree 79ff827378ddc973cfc5f375e0d0f9a4ccc9428f

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      console.log(event);
+      event.target.children[1].value
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
